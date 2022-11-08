bffe38c418c5f0648760a450769b660dc59957ca
Xq3331 <117724200+Xq3331@users.noreply.github.com>
2022-11-08 19:58:56 +0100

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,9 +17,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t size)
 	size_t	i;
 
 	i = 0;
+	if (size == 0)
+		return (0);
 	while (i < size - 1 && s1[i] == s2[i])
 		i++;
 	if (!(i < size) && s1[i - 1] == s2[i - 1])
 		return (0);
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
